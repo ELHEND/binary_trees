8e75d357228b8a98cffdc1b8a5175e6be798fc6c
ELHEND <alhnd683@gmail.com>
2023-08-29 11:21:54 -0700

    fv

--- 132-array_to_heap.c
+++ 132-array_to_heap.c
@@ -3,7 +3,8 @@
 /**
  *  * array_to_heap - builds a Max Binary Heap tree from an array
  *   *
- *    * @array: pointer to the first element of the array to be converted
+ *    * @array: pointer to the first element of  array to be converted
+ *
  *     * @size: number of elements in the array
  *      * Return: pointer to the root node of the created Binary Heap,
  *
@@ -12,15 +13,15 @@
 
 heap_t *array_to_heap(int *array, size_t size)
 {
-	size_t i;
+	size_t m;
 	heap_t *root = NULL;
 
 	if (!array)
 		return (NULL);
 
 	root = heap_insert(&root, array[0]);
-	for (i = 1; i < size; i++)
-		heap_insert(&root, array[i]);
+	for (m = 1; m < size; m++)
+		heap_insert(&root, array[m]);
 
 	return (root);
 }
