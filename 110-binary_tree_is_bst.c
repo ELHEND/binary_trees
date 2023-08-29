a734148bdd178ff7f4ee7d213d8527e9c8e6acfb
ELHEND <alhnd683@gmail.com>
2023-08-29 09:51:09 -0700

    hend

--- 110-binary_tree_is_bst.c
+++ 110-binary_tree_is_bst.c
@@ -3,7 +3,7 @@
 /**
  *  * isBST - Compare node in order to check if a given
  *   *
- *    * @tree: pointer to the root node of the tree to check
+ *    * @tree: pointer to  root node of the tree to check
  *     * @min: min value
  *      * @max: max value
  *       *
@@ -25,7 +25,8 @@ int isBST(const binary_tree_t *tree, int min, int max)
 }
 
 /**
- *  * binary_tree_is_bst - checks if a binary tree is a valid Binary Search tree
+ *  * binary_tree_is_bst - checks if a
+ *  binary tree is a valid Binary Search tree
  *   *
  *    * @tree: pointer to the root node of the tree to check
  *     * Return: 1 if success, 0 otherwise
