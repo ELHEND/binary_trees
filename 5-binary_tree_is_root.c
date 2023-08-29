0ef08ec0af6ad098c2f50ccffb906365919f0365
ELHEND <alhnd683@gmail.com>
2023-08-29 09:15:45 -0700

    hend

--- 5-binary_tree_is_root.c
+++ 5-binary_tree_is_root.c
@@ -4,7 +4,7 @@
  *  * binary_tree_is_root - checks if a given node is a root
  *   *
  *    * @node: pointer to  node to check
- *     * Return: 1 if node is a root, otherwise 0
+ *     * Return: 1 if node is  root, otherwise 0
  *
  *      * If node is NULL, return 0
  */
