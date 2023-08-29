a345b283fab540e7db75a5ea7d31775f1f755fe2
ELHEND <alhnd683@gmail.com>
2023-08-29 09:30:44 -0700

    hend

--- 14-binary_tree_balance.c
+++ 14-binary_tree_balance.c
@@ -1,13 +1,15 @@
 #include "binary_trees.h"
 
 /**
- *  * binary_tree_height_balance - measures the balance factor of a binary tree
- *   * @tree: pointer to  root node of the tree to measure the balance factor
+ *  * binary_tree_height_balance - measures
+ *  the balance factor of a binary tree
+ *   * @tree: pointer to  root node of the
+ *   tree to measure the balance factor
  *
  *    *
  *     * Return: height
  *      * If tree is NULL, return 0
- *       */
+ */
 
 size_t binary_tree_height_balance(const binary_tree_t *tree)
 {
