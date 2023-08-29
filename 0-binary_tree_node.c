02262bf85e04c29c3358a4638f8221212e6248f3
ELHEND <alhnd683@gmail.com>
2023-08-29 09:07:30 -0700

    hend

--- 0-binary_tree_node.c
+++ 0-binary_tree_node.c
@@ -2,7 +2,7 @@
 /**
  *  * binary_tree_node - creates a binary tree node
  *   *
- *    * @parent: pointer to the parent node
+ *    * @parent: pointer to parent node
  *     * @value: value to put in the node
  *      * Return: pointer to the new node
  *       *	or NULL on failure
