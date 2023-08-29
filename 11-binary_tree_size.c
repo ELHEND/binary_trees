6bc84fde145bcee0b234ba7f786c28aa7cc20957
ELHEND <alhnd683@gmail.com>
2023-08-29 09:26:25 -0700

    hend

--- 11-binary_tree_size.c
+++ 11-binary_tree_size.c
@@ -3,7 +3,7 @@
 /**
  *  * binary_tree_size - measures the size of a binary tree
  *   *
- *    * @tree: pointer to the root node of the tree to measure the size
+ *    * @tree: pointer to  root node of the tree to measure the size
  *     * Return: size or 0 if tree is NULL
  */
 
