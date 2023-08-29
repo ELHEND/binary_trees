664db0e1b60e20e5db12961abfde524e6a89fae4
ELHEND <alhnd683@gmail.com>
2023-08-29 09:25:26 -0700

    hend

--- 10-binary_tree_depth.c
+++ 10-binary_tree_depth.c
@@ -3,7 +3,7 @@
 /**
  *  * binary_tree_depth - measures the depth of a node in a binary tree
  *   *
- *    * @tree: pointer to the node to measure the depth
+ *    * @tree: pointer to  node to measure the depth
  *     * Return: 0 if tree is NULL
  */
 
