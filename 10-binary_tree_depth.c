57ba314eefd12ec8b16401aec6cc7c1cfd9badb3
ELHEND <alhnd683@gmail.com>
2023-08-30 08:53:40 -0700

    w

--- 10-binary_tree_depth.c
+++ 10-binary_tree_depth.c
@@ -1,7 +1,7 @@
 #include "binary_trees.h"
 
 /**
- *  * binary_tree_depth - measures the depth of a node in a binary tree
+ *  * binary_tree_depth - measures  depth of a node in a binary tree
  *   *
  *    * @tree: pointer to  node to measure the depth
  *     * Return: 0 if tree is NULL
